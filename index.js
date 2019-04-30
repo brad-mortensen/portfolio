b79eb384989be9ef7971b69eedc75371eace3410
Brad Mortensen <b.mort86@gmail.com>
2019-04-30 13:18:49 -0600

    scroll effects

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
 const upScroller = document.getElementById('up-scroller');
 const downScroller = document.getElementById('down-scroller');
 
 const nextSection = document.querySelector('.projects-container');
 
 downScroller.addEventListener('click', () => {
-  console.log('scroller clicked', document.activeElement);
-  nextSection.scrollIntoView({behavior: 'smooth'})
+	console.log('scroller clicked', document.activeElement);
+	nextSection.scrollIntoView({ behavior: 'smooth' });
+});
+upScroller.addEventListener('click', () => {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
