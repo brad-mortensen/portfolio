758f6469a4d78c99d16a5e2705305eecfcdc1bf5
Brad Mortensen <b.mort86@gmail.com>
2019-04-30 21:19:58 -0600

    updating js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const upScroller = document.getElementById('up-scroller');
 const downScroller = document.getElementById('down-scroller');
-
-const nextSection = document.querySelector('.projects-container');
+const nextSection = document.getElementsByClassName('main-container');
 
 downScroller.addEventListener('click', () => {
-	console.log('scroller clicked', document.activeElement);
+	console.log('scroller clicked');
 	nextSection.scrollIntoView({ behavior: 'smooth' });
 });
 upScroller.addEventListener('click', () => {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
+
+console.log('da window', window)
