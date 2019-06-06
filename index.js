ba79abdfe9946f0854d0b2fcdcbbe4305c0098c9
Brad Mortensen <b.mort86@gmail.com>
2019-06-06 16:37:50 -0600

    comments styles

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var upScroller = document.getElementById('up-scroller');
-var downScroller = document.getElementById('down-scroller');
-var nextSection = document.getElementById('main-container');
+// var upScroller = document.getElementById('up-scroller');
+// var downScroller = document.getElementById('down-scroller');
+// var nextSection = document.getElementById('main-container');
 
-window.addEventListener('load', () => {
-	if (window.scrollY == 0) {
-		upScroller.classList.add('toggler');
-	}
-});
-downScroller.addEventListener('click', () => {
-	window.scrollBy(0, window.innerHeight);
-});
-upScroller.addEventListener('click', () => {
-	window.scrollBy(0, -window.innerHeight);
-});
-window.onscroll = () => {
-	if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-		downScroller.classList.add('toggler');
-	} else {
-		downScroller.classList.remove('toggler');
-	}
-	if (window.scrollY == 0) {
-		upScroller.classList.add('toggler');
-	} else {
-		upScroller.classList.remove('toggler');
-	}
-};
+// window.addEventListener('load', () => {
+// 	if (window.scrollY == 0) {
+// 		upScroller.classList.add('toggler');
+// 	}
+// });
+// downScroller.addEventListener('click', () => {
+// 	window.scrollBy(0, window.innerHeight);
+// });
+// upScroller.addEventListener('click', () => {
+// 	window.scrollBy(0, -window.innerHeight);
+// });
+// window.onscroll = () => {
+// 	if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+// 		downScroller.classList.add('toggler');
+// 	} else {
+// 		downScroller.classList.remove('toggler');
+// 	}
+// 	if (window.scrollY == 0) {
+// 		upScroller.classList.add('toggler');
+// 	} else {
+// 		upScroller.classList.remove('toggler');
+// 	}
+// };
